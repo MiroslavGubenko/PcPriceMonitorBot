f91580d79e3a6afd0c40739284b8be0fc6336332
MiroslavH <miroslavgubenko@gmail.com>
2025-07-12 11:07:32 +0300

    add timeout

--- parsers/baseParser.js
+++ parsers/baseParser.js
@@ -29,10 +29,10 @@ async function parsePrice(storeConfig) {
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
     );
     await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
-    await page.waitForSelector(priceSelector);
+    await page.waitForSelector(priceSelector, { timeout: 60000 });
 
     const price = extractPrice(
-      await page.$eval(priceSelector, (el) => el.textContent)
+      await page.$eval(priceSelector, (el) => el.textContent.trim())
     );
     console.log(price);
     if (!price) {
